ac2bbaa1203b0c1345ed0417071d30723d206a1b
arttge <geyate@gmail.com>
2018-03-12 11:00:26 +0800

    Initial Commit

---
+++ countdownScreensaver/countdownScreensaverView.h
@@ -0,0 +1,13 @@
+//
+//  countdownScreensaverView.h
+//  countdownScreensaver
+//
+//  Created by yate on 2018/3/12.
+//  Copyright © 2018年 yate ge. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface countdownScreensaverView : ScreenSaverView
+
+@end
